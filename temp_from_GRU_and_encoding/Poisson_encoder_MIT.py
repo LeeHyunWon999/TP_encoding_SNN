600411bfde1e26a57cc6d56c877cd677cca7d2e2
leehyunwon999 <lhwlord@naver.com>
2024-07-21 09:13:08 +0000

    update

--- temp_from_GRU_and_encoding/Poisson_encoder_MIT.py
+++ temp_from_GRU_and_encoding/Poisson_encoder_MIT.py
@@ -6,31 +6,7 @@ import numpy as np
 import torch
 from spikingjelly.activation_based import neuron, encoding
 
-# 똑같은 TP 인코더, 근데 이제 2차원용으로 차원 확장된 기본버전
-
 # 입력모듈용으로 리팩토링 필요 : 얘만의 json이 필요한 것이 아니기 때문이며 텐서로 바꾸는 등의 작업이 따로 필요하다.
-
-
-# 인코딩용 뉴런 정의
-class TP_neuron(neuron.BaseNode) : 
-    
-    # 생성자
-    def __init__(self, tau, g, threshold = 1.0, reset = False, reset_value = 0.0, leaky = False) : 
-        super().__init__()
-        self.v_threshold = threshold
-        self.v_reset = None if reset is False else reset_value
-        self.leaky = leaky
-        self.tau = tau
-        self.g = g
-    
-    # 인코딩 스텝
-    def neuronal_charge(self, x: torch.Tensor) : 
-        if self.leaky : 
-            # LIF 뉴런 체계인 경우 leak 값인 tau를 이용하여 계산
-            self.v = np.exp(-1 / self.tau) * self.v + (x * self.g)
-        else : 
-            self.v = self.v + (x * self.g)
-    
 
 
 # json 읽어다가 반환(파일경로 없으면 에러띄우기)
@@ -58,11 +34,7 @@ def encode(json_data) :
     
     # 파일 형변환
     inputData = torch.tensor(inputData)
-    
-    
-    
-    # # 입력데이터는 [N, T] 형식인데, 인코더 뉴런은 [T, N, *] 을 기대한다. 따라서 축을 변경한다.
-    # inputData = inputData.transpose(1,0)
+
     
     encoded_list = []
     
