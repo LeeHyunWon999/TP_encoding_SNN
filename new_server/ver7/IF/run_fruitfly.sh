c27f54949ef83d0c3b969b2b5ad13464f4814f5e
LeeHyunWon999 <lee.hyunwon999@gmail.com>
2025-05-03 17:47:24 +0900

    .sh making

---
+++ new_server/ver7/IF/run_fruitfly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# burst
+python main.py ./config/train/
+
+# FTP
+python main.py ./config/train/
+
+# poisson
+python main.py ./config/train/
+
+# TP
+python main.py ./config/train/
+
+# TP_learnable
+python main.py ./config/train/
